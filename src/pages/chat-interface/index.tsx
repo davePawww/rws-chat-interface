bd03c0b89686d442899c24d56d70425d9630c012
DavePaurillo <paurillo.dave@gmail.com>
2026-05-25 23:38:06 +0800

    feat: edit message - store state and action for the edit message - added key prop to index.tsx and passed to MessageInput (trick) - wire it up to the onSelect edit message - message input sending updates because of these changes

--- src/pages/chat-interface/index.tsx
+++ src/pages/chat-interface/index.tsx
@@ -1,13 +1,16 @@
 import MessageInput from '@/pages/chat-interface/components/message-input';
 import MessageList from '@/pages/chat-interface/components/message-list';
 import UserDropdown from '@/pages/chat-interface/components/user-dropdown';
+import { useChatInterfaceStore } from '@/store/chat-interface.store';
 
 export default function ChatInterfacePage() {
+  const editingMessage = useChatInterfaceStore((s) => s.editingMessage);
+
   return (
     <>
       <UserDropdown />
       <MessageList />
-      <MessageInput />
+      <MessageInput key={editingMessage?.id ?? 'new'} />
     </>
   );
 }
